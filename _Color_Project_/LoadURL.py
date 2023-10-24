18a4a10b0daf15616f220d60ad03e56d4f7409d9
dyno511 <104981764+dyno511@users.noreply.github.com>
2023-10-24 13:07:30 +0700

    Chỉnh sửa Giao Diện ngày 23-10

--- _Color_Project_/LoadURL.py
+++ _Color_Project_/LoadURL.py
@@ -5,7 +5,8 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 import requests
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWebChannel import QWebChannel
-
+from PyQt5.QtGui import QIcon  # Import QIcon
+from PyQt5.QtGui import QCursor  # Import QCursor from QtGui
 def CheckIsURL(url):
     try:
         response = requests.get(url)
@@ -34,6 +35,40 @@ class BrowserWindow(QMainWindow):
         self.setWindowFlag(Qt.FramelessWindowHint)
         self.setCentralWidget(self.browser)
         self.browser.setStyleSheet("background-color: #f0f0f0;")
+
+
+
+        # # Create and style the Quit button
+        # quit_button = QPushButton("Quit", self)
+        # quit_button.clicked.connect(QApplication.instance().quit)
+        # quit_button.setGeometry(1775, 7, 80, 30)
+        # quit_button.setStyleSheet(
+        #     """
+        #     background-color: #fff;
+        #     border: 2px solid #000;
+        #     color: #000;
+        #     font-family: Inter,sans-serif;
+        #     font-size: 16px;
+        #     font-weight: 600;
+        #     text-align: center;
+        #     padding: 0 17px;
+        #     min-width: 80px;
+        #     height: 48px;
+        #     justify-content: center;
+        #     align-items: center;
+        #     display: inline-flex;
+        #     cursor: pointer;
+        #     user-select: none;
+        #     -webkit-user-select: none;
+        #     touch-action: manipulation;
+        #     margin-left:10px
+        #     """
+        # )
+
+        # quit_button.setCursor(QCursor(Qt.PointingHandCursor))  # Change cursor to a hand on hover
+
+        
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
